3468e345850666b4f66f4b152837909117c73792
“githe <562004191@qq.com>
2019-05-14 11:48:33 +0800

    运算符重载

---
+++ 5.14.2/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+const int SIZE = 3;
+
+class atype {
+    int a[SIZE];
+public:
+   atype( ) {
+       register int i;
+       for(i=0; i<SIZE; i++) a[i] = i;
+  }
+  int &operator[](int i);
+};
+int& atype::operator[](int i){
+    if(i>=3 || i<0){
+        cout<<"Index value of 3 is out-of-bounds."<<endl;
+        exit(0);
+    }else{
+        return a[i];
+    }
+}
+
+int main( )
+{
+   atype ob;
+   cin >> ob[1];
+   ob[2] = ob[1];  // 下标运算符[]出现在赋值运算符的左边和右边
+   cout << ob[2];
+   ob[3] = 44; // 产生运行时错误，下标3超出了数组边界
+   return 0;
+}
